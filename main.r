3e309da95dec263fbe1af6f77316926c0267ba0d
Timothy Healey <87385121+timhealey3@users.noreply.github.com>
2023-11-28 15:06:39 -0500

    clean all data

--- main.r
+++ main.r
@@ -7,3 +7,18 @@ gas <- read_excel(excel_file, sheet = "Natural Gas")
 petro <- read_excel(excel_file, sheet = "Petroleum")
 nuclear <- read_excel(excel_file, sheet = "Nuclear")
 renew <- read_excel(excel_file, sheet="Total Renewable Energy")
+# clean data
+
+fixed_data <- function(df) {
+  cleaned_df <- na.omit(df)
+  colnames(cleaned_df) <- cleaned_df[1,]
+  cleaned_df <- cleaned_df[-1, ] 
+}
+
+coal <- fixed_data(coal)
+gas <- fixed_data(gas)
+petro <- fixed_data(petro)
+nuclear <- fixed_data(nuclear)
+renew <- fixed_data(renew)
+
+# Linear Regression
